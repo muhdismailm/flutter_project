56d2a56f603adc4003870a7b2ac440161e02c6b7
muhdismailm <muhammedismailm772@gmail.com>
2025-03-08 13:49:42 -0800

    splash screen

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+const tPrimaryColor = Color.fromARGB(0, 15, 112, 238);
+const tSecondaryColor = Color.fromRGBO(0, 69, 114, 0);
--- lib/src/features/authentication/screens/splash_screen/splash_screen.dart
+++ lib/src/features/authentication/screens/splash_screen/splash_screen.dart
@@ -1,17 +1,37 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:login_1/src/constants/colors.dart';
 import 'package:login_1/src/constants/image_string.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  bool animate = false;
+
+  @override
+  void initState(){
+    startAnimation();
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children:[
-          const Positioned(
-          top: 0,
-          left: 0,
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 1600),
+          top: animate ? 0 :-30,
+          left: animate ? 0 :-30,
           child: Image(image: AssetImage(tSplashIcon)),
           ),
         Positioned(
@@ -29,8 +49,26 @@ class SplashScreen extends StatelessWidget {
           bottom: 40,
           child: Image(image: AssetImage(tSplashworker)),
           ),
+          Positioned(
+            bottom: 40,
+            right: 40,
+            child: Container(
+              width: 50 ,
+              height: 100,
+              color: tPrimaryColor,
+            ),
+          )
 
         ]
     ));
   }
+}
+
+Future startAnimation() async{
+  await Future.delayed(Duration(milliseconds: 500));
+  setState(){
+  var animate =true;
+  }
+  //  await Future.delayed(Duration(milliseconds: 5000));
+  //  Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => welcome_screen,))
 }
